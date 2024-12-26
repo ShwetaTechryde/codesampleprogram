42349869058ce8bc0d4f62dee029b9798202f148
Dell <sweta18singh@gmail.com>
2024-12-26 00:34:18 -0800

    changes

---
+++ Interface/IVehcileOperations.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using codesampleprogram.Models;
+
+namespace codesampleprogram.Interface
+{
+    public interface IVehcileOperations
+    {
+        public void  AddVehcile();
+
+        public void DeleteVehcile(int vehid);
+        public void UpdateVehcile(int vehid);
+    }
+}
